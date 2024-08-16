e1810a63a564b6912eee37c1fbf3df111abeb503
Rosa Kang <therosessom@gmail.com>
2024-08-15 19:09:50 -0700

    5:53 Time To Program

--- server/index.js
+++ server/index.js
@@ -18,3 +18,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /** ROUTES */
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
