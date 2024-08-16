50ed9157180cd757ed8e7e12707df2456ba8d1bd
Rosa Kang <therosessom@gmail.com>
2024-08-16 13:27:57 -0700

    debug just link tailwind  w/o using postcss method

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./*.{html,js,ts,jsx}', './src/*.jsx',],
+  content: ['./*.{html,js,ts,jsx}', './src/**/*.jsx',],
   theme: {
     extend: {},
   },
