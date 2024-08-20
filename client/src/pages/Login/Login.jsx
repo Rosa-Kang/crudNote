aa591678ebcf8710c0dd1c08af0a8c3ddb5c9072
Rosa Kang <therosessom@gmail.com>
2024-08-20 10:56:00 -0700

    reset tailwind and vite config, login page setup

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
+import PasswordInput from '../../components/Input/PasswordInput';
 import { Link } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid Email address.");
+      return;
+    }
+
+    if(!password){
+      setError("Please enter the password.")
+    }
+
+    setError("")
+  }
+
   return (
     <div>
       <Navbar  />
 
       <div className='flex items-center justify-center mt-28'>
-        <div>
-          <form onSubmit={()=> {}}>
+        <div className='w-96 border rounded bg-white px-7 py-10'>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
 
-            <input type="text" className="input-box" placeholder='Email' />
+            <input 
+              type="text" 
+              className="input-box" 
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+             />
+
+            <PasswordInput
+              value= {password}
+              onChange={(e) => {setPassword(e.target.value); console.log(error)}}
+             />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button type="submit" className="btn-primary">
                 Login
@@ -21,7 +55,7 @@ const Login = () => {
             <p className="text-sm text-center mt-4">
               Not registerred yet?{" "}
 
-              <Link to="/signup" className=""> Create an Account</Link>
+              <Link to="/signup" className="font-medium text-primary underline"> Create an Account</Link>
             </p>
           </form>
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./*.{html,js,ts,jsx}', './src/**/*.jsx',],
+  content: ["./*.{html,js,ts,jsx}", "./src/**/*.jsx", "*"],
   theme: {
-    extend: {},
+    extend: {
+      // Colors used in the project
+      colors: {
+        primary: "#2885ff",
+        secondary: "#ef863e"
+      }
+    },
   },
   plugins: [],
 }
