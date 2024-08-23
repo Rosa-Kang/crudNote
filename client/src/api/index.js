a49ddfef4c72e047faad494da3844bb7c280ad94
Rosa Kang <therosessom@gmail.com>
2024-08-23 12:05:04 -0700

    add api in client to connect server

---
+++ client/src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: 'http://localhost:5001/',
+    timeout: 10000,
+    headers: {
+        "Content-Type" : "application/json"
+    },
+});
+
+
+API.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem("token");
+        if(accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default API;
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import PasswordInput from '../../components/Input/PasswordInput';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
+import API from '../../api';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -25,6 +28,23 @@ const Login = () => {
     setError("")
 
     // Login API Call
+  try {
+    const response = await API.post("/login", {
+      email : email,
+      password: password,
+    });
+
+    if(response.data && response.data.accessToken) {
+      localStorage.setItem("token", response.data.accessToken);
+      navigate("/")
+    }
+  } catch (error) {
+    if(error.response && error.response.data && error.response.data.message) {
+      setError(error.response.data.message);
+    } else {
+      setError("An unexpected error occrred. Please try again.")
+    }
+  }
   }
 
   return (
